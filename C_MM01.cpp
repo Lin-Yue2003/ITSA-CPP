62271b479465c64a5462e6950cf2fe6dd77a0c40
Yue <131417862+Lin-Yue2003@users.noreply.github.com>
2024-06-27 10:15:12 +0800

    Update C_MM01.cpp

--- C_MM01.cpp
+++ C_MM01.cpp
@@ -2,10 +2,10 @@
 #include <iomanip>
 using namespace std;
 int main(void){
-    int h,tl,bl;
+    float h,tl,bl;
     float a;
-    while (cin>>h>>tl>>bl){
-        cout<<"Trapezoid area:"<< fixed <<setprecision(1)<<(tl+bl)/2.0*h<<"\n";
+    while (cin>>tl>>bl>>h){
+        cout<<"Trapezoid area:"<< fixed <<setprecision(1)<<((tl+bl)*h/2.0)<<"\n";
     }
     return 0;
 }
